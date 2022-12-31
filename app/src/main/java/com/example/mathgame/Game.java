2db0f98d05250d56b7ae06df224e34ce3efcef4a
MingkwanChws <58773342+MingkwanChws@users.noreply.github.com>
2022-12-31 20:46:49 +0700

    Result Activity logic
    connect between Game Activity, Result Activity and Main Activity.
    
    in addition case

--- app/src/main/java/com/example/mathgame/Game.java
+++ app/src/main/java/com/example/mathgame/Game.java
@@ -2,11 +2,13 @@ package com.example.mathgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Locale;
 import java.util.Random;
@@ -79,8 +81,18 @@ public class Game extends AppCompatActivity {
             public void onClick(View view) {
 
                 answer.setText("");
-                gameContinue();
                 resetTimer();
+
+                if(userLife <= 0){
+                    Toast.makeText(getApplicationContext(), "Game Over", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(Game.this, Result.class);
+                    intent.putExtra("score", userScore);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    gameContinue();
+                }
 
             }
         });
